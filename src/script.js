da2bb85f7ff09793acea7e9da59c6c69c12075d2
hellodayy <ohlemon0678@gmail.com>
2024-07-16 14:52:40 +0700

    update axios city name and temp

--- src/script.js
+++ src/script.js
@@ -1,9 +1,23 @@
-function changeCity(event) {
+function changeWeather(response) {
+  let cityTitle = document.querySelector("#city");
+  let temp = document.querySelector("#temp");
+  let currentTemp = Math.round(response.data.temperature.current);
+
+  cityTitle.innerHTML = response.data.city;
+  temp.innerHTML = currentTemp;
+}
+
+function getCity(city) {
+  let apiKey = "fdb1b4026fa299784f6o27t429cd3399";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(changeWeather);
+}
+
+function submitHandler(event) {
   event.preventDefault();
-  let city = document.querySelector("#city");
   let inputCity = document.querySelector("#input-city");
-  city.innerHTML = inputCity.value;
+  getCity(inputCity.value);
 }
 
 let btn = document.querySelector("#input-button");
-btn.addEventListener("click", changeCity);
+btn.addEventListener("click", submitHandler);
