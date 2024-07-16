1545593120fb11389a5eda51be1bbe0d37b5a494
hellodayy <ohlemon0678@gmail.com>
2024-07-16 15:47:44 +0700

    update icon,date,wind,speed

--- src/script.js
+++ src/script.js
@@ -3,18 +3,22 @@ function changeWeather(response) {
   let temp = document.querySelector("#temp");
   let currentTemp = Math.round(response.data.temperature.current);
   let date = new Date(response.data.time * 1000);
-  let dateCurrent = document.querySelector("#dateCurrent");
+  let dateCurrent = document.querySelector("#date-current");
   let humidity = document.querySelector("#humidity");
   let wind = document.querySelector("#wind");
   let currentHumidity = response.data.temperature.humidity;
   let currentWind = response.data.wind.speed;
+  let weatherDescription = document.querySelector("#weather-description");
+  let emoji = document.querySelector("#emoji-temp");
+  let image = response.data.condition.icon_url;
 
-  console.log(response.data);
+  weatherDescription.innerHTML = response.data.condition.description;
   humidity.innerHTML = `${currentHumidity}%`;
   wind.innerHTML = `${currentWind}km/h`;
   dateCurrent.innerHTML = dateTime(date);
   cityTitle.innerHTML = response.data.city;
   temp.innerHTML = currentTemp;
+  emoji.innerHTML = `<img src="${image}" alt="icon" class="emoji-temp" />`;
 }
 
 function dateTime(date) {
