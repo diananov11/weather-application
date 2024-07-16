98d6c6c8dbe1beee5ca85c0bc390c3d13c67c114
hellodayy <ohlemon0678@gmail.com>
2024-07-16 15:26:14 +0700

    update data API

--- src/script.js
+++ src/script.js
@@ -2,9 +2,40 @@ function changeWeather(response) {
   let cityTitle = document.querySelector("#city");
   let temp = document.querySelector("#temp");
   let currentTemp = Math.round(response.data.temperature.current);
+  let date = new Date(response.data.time * 1000);
+  let dateCurrent = document.querySelector("#dateCurrent");
+  let humidity = document.querySelector("#humidity");
+  let wind = document.querySelector("#wind");
+  let currentHumidity = response.data.temperature.humidity;
+  let currentWind = response.data.wind.speed;
 
+  console.log(response.data);
+  humidity.innerHTML = `${currentHumidity}%`;
+  wind.innerHTML = `${currentWind}km/h`;
+  dateCurrent.innerHTML = dateTime(date);
   cityTitle.innerHTML = response.data.city;
   temp.innerHTML = currentTemp;
+}
+
+function dateTime(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function getCity(city) {
@@ -21,3 +52,5 @@ function submitHandler(event) {
 
 let btn = document.querySelector("#input-button");
 btn.addEventListener("click", submitHandler);
+
+getCity("palembang");
